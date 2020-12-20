95d3475131c7af2a56d8a205adad565b64f11c30
tehzevo <tehzevo@users.noreply.github.com>
2020-12-20 00:06:06 -0500

    Change name to aegis-distribute

--- server.js
+++ server.js
@@ -20,20 +20,24 @@ var auto = process.env.AUTO;
 auto = auto != null ? (auto.toLowerCase() == "true") : false;
 
 var port = process.env.PORT || "80";
+port = parseInt(port)
+
+console.log("sync", sync)
+console.log("auto", auto)
 
 //run each in succession
-async function syncFunction(urls)
+async function syncFunction(urls, data)
 {
   for(var url of urls)
   {
-    await protopost(url);
+    await protopost(url, "", data);
   }
 }
 
 //run each async
-async function asyncFunction(urls)
+async function asyncFunction(urls, data)
 {
-  return Promise.all(urls.map((e) => protopost(e)));
+  return Promise.all(urls.map((e) => protopost(e, "", data)));
 }
 
 var runFunction = sync ? syncFunction : asyncFunction;
@@ -53,5 +57,5 @@ if(auto)
 else
 {
   //TODO: return stuff
-  new ProtoPost({}, (data) => runFunction(urls, "", data)).start();
+  new ProtoPost({}, (data) => runFunction(urls, data)).start(port);
 }
